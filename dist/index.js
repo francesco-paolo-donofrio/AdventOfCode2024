506ce2e293eb894fe2510ca58ebbf8202b62ad1f
Frankosviz <geom.francescopaolo@gmail.com>
2025-01-24 17:32:55 +0100

    Correct solution to see the array

--- dist/index.js
+++ dist/index.js
@@ -1,62 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-function main() {
-    const filePath = "C:\\Users\\newFr\\OneDrive\\Desktop\\InputExercise.txt";
-    try {
-        const fileContent = fs_1.default.readFileSync(filePath, 'utf-8');
-        const lines = fileContent.split(/\r?\n/);
-        console.log("File letto correttamente:", lines);
-        let totale = 0;
-        for (const line of lines) {
-            if (line.trim() === "")
-                continue;
-            const numeri = line
-                .split(" ")
-                .map(s => parseInt(s))
-                .filter(n => !isNaN(n));
-            console.log("Numeri analizzati:", numeri.join(" "));
-            if (isSicuroConDampener(numeri)) {
-                totale++;
-            }
-        }
-        console.log("TOTALE: " + totale);
+const fs = require('fs');
+const filePath = "C:\\Desktop_nuovo\\esercizi-boolean\\Typescript\\AdventOfCode2024\\dist\\file-di-prova.txt";
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error('Errore nella lettura del file:', err);
+        return;
     }
-    catch (error) {
-        console.error("Errore durante la lettura del file:", error);
-    }
-}
-function isSicuroConDampener(numeri) {
-    if (isSicuro(numeri)) {
-        return true;
-    }
-    for (let i = 0; i < numeri.length; i++) {
-        const numeriModificato = [...numeri];
-        numeriModificato.splice(i, 1);
-        if (isSicuro(numeriModificato)) {
-            return true;
-        }
-    }
-    return false;
-}
-function isSicuro(numeri) {
-    if (numeri.length < 2)
-        return false;
-    const crescente = numeri[0] < numeri[1];
-    for (let i = 0; i < numeri.length - 1; i++) {
-        const differenza = numeri[i + 1] - numeri[i];
-        if (Math.abs(differenza) > 3 || differenza === 0) {
-            return false;
-        }
-        if ((crescente && differenza < 0) || (!crescente && differenza > 0)) {
-            return false;
-        }
-    }
-    return true;
-}
-main();
-isSicuro([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]);
+    console.log('Contenuto del file:');
+    console.log(data);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,87 @@
-import fs from 'fs';
+const fs = require('fs');
+
+const filePath = "C:\\Desktop_nuovo\\esercizi-boolean\\Typescript\\AdventOfCode2024\\dist\\file-di-prova.txt";
+
+fs.readFile(filePath, 'utf-8', (err : any, data : any) => {
+    if (err) {
+        console.error('Errore nella lettura del file:', err);
+        return;
+    }
+
+    console.log('Contenuto del file:');
+    console.log(data);
+});
 
 // Advent number.1
 
 // Advent number.2
 
-function main(): void {
-    const filePath = "C:\\Users\\newFr\\OneDrive\\Desktop\\InputExercise.txt";
+// function main(): void {
+//     const filePath = "C:\\Desktop_nuovo\\esercizi-boolean\\Typescript\\AdventOfCode2024\\dist\\file-di-prova.txt";
+//     const fileContent = fs.readFileSync(filePath, 'utf-8');
+//          const lines = fileContent.split(/\r?\n/);
+//          console.log("File letto correttamente:", lines);
+//     for (let i = 0; i < 10; i++) {
+//         console.log("Riga " + i + ": " + lines[i]);
+//     }
+//     // try {
+//     //     // Leggi il contenuto del file
+//     //     const fileContent = fs.readFileSync(filePath, 'utf-8');
+//     //     const lines = fileContent.split(/\r?\n/);
+//     //     console.log("File letto correttamente:", lines);
 
-    try {
-        // Leggi il contenuto del file
-        const fileContent = fs.readFileSync(filePath, 'utf-8');
-        const lines = fileContent.split(/\r?\n/);
-        console.log("File letto correttamente:", lines);
+//     //     let totale = 0;
 
-        let totale = 0;
+//     //     for (const line of lines) {
+//     //         if (line.trim() === "") continue; // Ignora righe vuote
 
-        for (const line of lines) {
-            if (line.trim() === "") continue; // Ignora righe vuote
+//     //         const numeri = line
+//     //             .split(" ")
+//     //             .map(s => parseInt(s))
+//     //             .filter(n => !isNaN(n)); // Filtra eventuali valori NaN
 
-            const numeri = line
-                .split(" ")
-                .map(s => parseInt(s))
-                .filter(n => !isNaN(n)); // Filtra eventuali valori NaN
+//     //         console.log("Numeri analizzati:", numeri.join(" "));
 
-            console.log("Numeri analizzati:", numeri.join(" "));
+//     //         if (isSicuroConDampener(numeri)) {
+//     //             totale++;
+//     //         }
+//     //     }
+//     //     console.log("TOTALE: " + totale);
+//     // } catch (error) {
+//     //     console.error("Errore durante la lettura del file:", error);
+//     // }
+// }
 
-            if (isSicuroConDampener(numeri)) {
-                totale++;
-            }
-        }
-        console.log("TOTALE: " + totale);
-    } catch (error) {
-        console.error("Errore durante la lettura del file:", error);
-    }
-}
+// function isSicuroConDampener(numeri: number[]): boolean {
+//     if (isSicuro(numeri)) {
+//         return true;
+//     }
+//     for (let i = 0; i < numeri.length; i++) {
+//         const numeriModificato = [...numeri];
+//         numeriModificato.splice(i, 1); // Rimuove un elemento
+//         if (isSicuro(numeriModificato)) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 
-function isSicuroConDampener(numeri: number[]): boolean {
-    if (isSicuro(numeri)) {
-        return true;
-    }
-    for (let i = 0; i < numeri.length; i++) {
-        const numeriModificato = [...numeri];
-        numeriModificato.splice(i, 1); // Rimuove un elemento
-        if (isSicuro(numeriModificato)) {
-            return true;
-        }
-    }
-    return false;
-}
+// function isSicuro(numeri: number[]): boolean {
+//     if (numeri.length < 2) return false; // Una sequenza con meno di due numeri non può essere sicura
 
-function isSicuro(numeri: number[]): boolean {
-    if (numeri.length < 2) return false; // Una sequenza con meno di due numeri non può essere sicura
+//     const crescente = numeri[0] < numeri[1];
+//     for (let i = 0; i < numeri.length - 1; i++) {
+//         const differenza = numeri[i + 1] - numeri[i];
 
-    const crescente = numeri[0] < numeri[1];
-    for (let i = 0; i < numeri.length - 1; i++) {
-        const differenza = numeri[i + 1] - numeri[i];
-
-        if (Math.abs(differenza) > 3 || differenza === 0) {
-            return false;
-        }
-        if ((crescente && differenza < 0) || (!crescente && differenza > 0)) {
-            return false;
-        }
-    }
-    return true;
-}
+//         if (Math.abs(differenza) > 3 || differenza === 0) {
+//             return false;
+//         }
+//         if ((crescente && differenza < 0) || (!crescente && differenza > 0)) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
 // Esegui la funzione principale
-main();
-isSicuro([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]);
+// console.log(main());
